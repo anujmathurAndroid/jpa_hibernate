e290ffa8278b55c3c92103b043e40866debe7055
Anuj Mathur <amathur@nectarcorp.com>
2025-01-14 18:23:29 +0530

    Add getAllNameList Api

--- src/main/java/com/anuj/learn/learn_jpa_hibernate/ApiController.java
+++ src/main/java/com/anuj/learn/learn_jpa_hibernate/ApiController.java
@@ -1,4 +1,13 @@
 package com.anuj.learn.learn_jpa_hibernate;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
+
+    @GetMapping("/all")
+    public String getAllNameList(){
+        return "Hello World";
+    }
 }
