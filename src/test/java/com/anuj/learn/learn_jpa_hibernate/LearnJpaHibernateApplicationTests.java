622e383ee156c515a9a511b4da1e2daa6f65f647
Anuj Mathur <amathur@nectarcorp.com>
2025-01-14 18:09:28 +0530

    initial commit

---
+++ src/test/java/com/anuj/learn/learn_jpa_hibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.learn.learn_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
